a7e659a03ce8f9361fc250c84d0c54218095bdff
Dolapo24 <adedolapovictoria927@gmail.com>
2023-03-11 10:29:22 +0100

    main

--- commerce/settings.py
+++ commerce/settings.py
@@ -55,9 +55,8 @@ ROOT_URLCONF = 'commerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-        ],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
