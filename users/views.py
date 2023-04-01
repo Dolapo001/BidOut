f6ff4cf7b6d1cbda96bb41545013b5e7355dacb9
Dolapo24 <adedolapovictoria927@gmail.com>
2023-04-01 02:42:06 +0100

    main

--- users/views.py
+++ users/views.py
@@ -9,10 +9,10 @@ from users.decorators import login_excluded
 from users.models import User
 
 
-@login_excluded(redirect_to="home")
 def dashboard(request):
     return render(request, "users/dashboard.html")
 
+# Create your views here.
 
 @login_excluded(redirect_to="home")
 def login_view(request):
@@ -26,11 +26,11 @@ def login_view(request):
         # Check if authentication successful
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse("Home"))
+            messages.success(request, "Logged in successfully")
+            return HttpResponseRedirect(reverse("home"))
         else:
-            return render(request, "users/login.html", {
-                "message": "Invalid username and/or password."
-            })
+            messages.error(request, "Error logging in")
+            return render(request, "users/login.html")
     else:
         return render(request, "users/login.html")
 
@@ -38,12 +38,15 @@ def login_view(request):
 @login_required(login_url="login")
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse("Home"))
+    messages.info(request, "Successfully logged out")
+    return HttpResponseRedirect(reverse("home"))
 
 
 @login_excluded(redirect_to="home")
-def register(request):
+def register(request, **extra_fields):
     if request.method == "POST":
+        first_name = request.POST["first-name"]
+        last_name = request.POST["last-name"]
         username = request.POST["username"]
         email = request.POST["email"]
 
@@ -51,19 +54,20 @@ def register(request):
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
-            return render(request, "users/signup.html", {
-                "message": "Passwords must match."
-            })
+            messages.error(request, "Passwords don't match")
+            return render(request, "users/signup.html")
 
         # Attempt to create new user
         try:
-            user = User.objects.create_user(username, email, password)
+            # adding first name and last name using extra_fields to the User fields
+            extra_fields = {**extra_fields, "first_name": first_name, "last_name": last_name}
+            user = User.objects.create_user(username, email, password, **extra_fields)
             user.save()
         except IntegrityError:
-            return render(request, "users/signup.html", {
-                "message": "Username already taken."
-            })
+            messages.info(request, "Username has already been taken")
+            return render(request, "users/signup.html")
         login(request, user)
-        return HttpResponseRedirect(reverse("Home"))
+        messages.success(request, "Logged in successfully")
+        return HttpResponseRedirect(reverse("home"))
     else:
         return render(request, "users/signup.html")
