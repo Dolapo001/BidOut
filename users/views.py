8236f482a524d927bb5080cc57b0c8911d435bf3
Dolapo24 <adedolapovictoria927@gmail.com>
2023-07-11 20:12:10 +0100

    main

--- users/views.py
+++ users/views.py
@@ -1,71 +1,36 @@
-from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.db import IntegrityError
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, reverse
+from django.contrib.auth import authenticate, login, get_user_model, logout
+from django.shortcuts import render, redirect
+from .models import User
 
-from users.decorators import login_excluded
-from users.models import User
+User = get_user_model()
 
 
 def dashboard(request):
     return render(request, "users/dashboard.html")
 
-# Create your views here.
 
 def login_view(request):
-    if request.method == "POST":
 
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    if request.method == 'POST':
+        email_or_username = request.POST['email_or_username']
+        password = request.POST['password']
+        try:
+            user = User.objects.get(email=email_or_username)
+        except User.DoesNotExist:
+            print('Username or Email does not exist')
+        user = authenticate(request, email=email_or_username, password=password)
 
-        # Check if authentication successful
         if user is not None:
             login(request, user)
-            messages.success(request, "Logged in successfully")
-            return HttpResponseRedirect(reverse("home"))
+            return redirect('dashboard')
         else:
-            messages.error(request, "Error logging in")
-            return render(request, "users/login.html")
-    else:
-        return render(request, "users/login.html")
+            print('Username/email or password is incorrect')
+
+    return render(request, 'users/login.html')
 
 
-@login_required(login_url="login")
 def logout_view(request):
     logout(request)
-    messages.info(request, "Successfully logged out")
-    return HttpResponseRedirect(reverse("home"))
-
-
-def register(request, **extra_fields):
-    if request.method == "POST":
-        first_name = request.POST["first-name"]
-        last_name = request.POST["last-name"]
-        username = request.POST["username"]
-        email = request.POST["email"]
-
-        # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
-        if password != confirmation:
-            messages.error(request, "Passwords don't match")
-            return render(request, "users/signup.html")
-
-        # Attempt to create new user
-        try:
-            # adding first name and last name using extra_fields to the User fields
-            extra_fields = {**extra_fields, "first_name": first_name, "last_name": last_name}
-            user = User.objects.create_user(username, email, password, **extra_fields)
-            user.save()
-        except IntegrityError:
-            messages.info(request, "Username has already been taken")
-            return render(request, "users/signup.html")
-        login(request, user)
-        messages.success(request, "Logged in successfully")
-        return HttpResponseRedirect(reverse("home"))
-    else:
-        return render(request, "users/signup.html")
+    return redirect('login')
